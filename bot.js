b6dd8c815ed9ab56b37b8549182a78d75e94290f
Kimmo Tamio <kimmo@kimmotamio.fi>
2016-06-26 07:01:32 +0000

    Added all of koodiluola servers in /servut

--- bot.js
+++ bot.js
@@ -81,13 +81,31 @@ bot.onText(/\/servut$/, function (msg) {
       console.log("lataus onnistui - jb");
       bot.sendPhoto(chatId, kuva4, {caption: "join.koodiluola.eu:27031"});
   });
+  lataus("http://cache.www.gametracker.com/server_info/176.126.84.204:27015/b_350_20_5A6C3E_383F2D_D2E1B5_2E3226.png", "kz.png", function(){
+      console.log("lataus onnistui - kz");
+      bot.sendPhoto(chatId, kuva5, {caption: "176.126.84.204:27015"});
+  });
+  lataus("http://cache.www.gametracker.com/server_info/176.126.84.190:27015/b_350_20_5A6C3E_383F2D_D2E1B5_2E3226.png", "kitsune.png", function(){
+      console.log("lataus onnistui - kitsune");
+      bot.sendPhoto(chatId, kuva6, {caption: "176.126.84.190:27015"});
+  });
+  lataus("http://cache.www.gametracker.com/server_info/31.220.5.130:27015/b_350_20_5A6C3E_383F2D_D2E1B5_2E3226.png", "mesa.png", function(){
+      console.log("lataus onnistui - mesa");
+      bot.sendPhoto(chatId, kuva7, {caption: "31.220.5.130:27015"});
+  });
+  lataus("http://cache.www.gametracker.com/server_info/176.126.85.18:27015/b_350_20_5A6C3E_383F2D_D2E1B5_2E3226.png", "mesa2.png", function(){
+      console.log("lataus onnistui - mesa2");
+      bot.sendPhoto(chatId, kuva8, {caption: "176.126.85.18:27015"});
+  });
   // From file
   var kuva = "bhop.png",
   kuva2 = "surf.png",
   kuva3 = "arena.png",
   kuva4 = "jb.png",
-  kuva5 = "",
-  kuva6 = "";
+  kuva5 = "kz.png",
+  kuva6 = "kitsune.png",
+  kuva7 = "mesa.png",
+  kuva8 = "mesa2.png";
 });
 
 bot.onText(/\/help$/, function (msg, match) {
